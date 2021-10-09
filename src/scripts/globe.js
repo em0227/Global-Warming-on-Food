4b58057236df8d2e8927f48876a30e6a5ef6b85b
Emily Wu <emilywu0227@gmail.com>
2021-10-09 09:09:51 -0400

    added the graticule but it makes the land and sea color lighter

--- src/scripts/globe.js
+++ src/scripts/globe.js
@@ -2,22 +2,25 @@
 
     function Globe(result) {
 
-        const s = 450
-        const radius = 225
+
+        
+        const radius = 400
         this.degree = 0
 
         this.canvas = document.getElementById("map-canvas");
         const c = this.canvas.getContext('2d');
 
-        this.projection = d3.geoOrthographic().scale(radius).translate([s / 2, s / 2]);
+        this.projection = d3.geoOrthographic().scale(radius).precision(0.2).translate([600, 500]);
         
         const path = d3.geoPath(this.projection, c);
+
+        
 
         // Draw the seas.
         c.lineWidth = 1.5;
         c.fillStyle = "aliceblue";
         c.beginPath();
-        c.arc(s / 2, s / 2, radius, 0, 2 * Math.PI);
+        c.arc(600, 500, radius, 0, 2 * Math.PI);
         c.fill();
         c.stroke();
 
@@ -29,18 +32,24 @@
         c.fill();
         c.stroke();
 
+        //the gratitcule
+        graticule = d3.geoGraticule10()
+        c.beginPath(), path(graticule), c.strokeStyle = "#ccc", c.stroke();
+
+        
         
         setInterval(() => {
             this.projection.rotate([this.change(), 0])
-            console.log(this.degree)
+
+            
             c.lineWidth = 1.5;
             c.fillStyle = "aliceblue";
             c.beginPath();
-            c.arc(s / 2, s / 2, radius, 0, 2 * Math.PI);
+            c.arc(600, 500, radius, 0, 2 * Math.PI);
             c.fill();
             c.stroke();
 
-            // Draw the land.
+            
             c.lineWidth = 0.35;
             c.fillStyle = "mintcream";
             c.beginPath();
@@ -48,6 +57,10 @@
             c.fill();
             c.stroke();
 
+            graticule = d3.geoGraticule10()
+            c.beginPath(), path(graticule), c.strokeStyle = "#ccc", c.stroke();
+
+            
         }, 500)
 
     }
