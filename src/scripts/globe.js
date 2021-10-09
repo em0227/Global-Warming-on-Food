ecbfe937ff69b2d654a59c3b1b11bb7fb9d44695
Emily Wu <emilywu0227@gmail.com>
2021-10-09 17:25:41 -0400

    refactor globe.js to cleaner code

--- src/scripts/globe.js
+++ src/scripts/globe.js
@@ -2,80 +2,51 @@
 
     function Globe(result) {
 
-
-        
-        const radius = 400
-        this.degree = 0
-
-        this.canvas = document.getElementById("map-canvas");
-        const c = this.canvas.getContext('2d');
-
+        const canvas = document.getElementById("map-canvas");
+        const ctx = canvas.getContext('2d');
+        const radius = 400;
         this.projection = d3.geoOrthographic().scale(radius).precision(0.2).translate([600, 500]);
-        
-        const path = d3.geoPath(this.projection, c);
+        const path = d3.geoPath(this.projection, ctx);
+        this.degree = 0;
 
-        
-
-        // Draw the seas.
-        c.lineWidth = 1.5;
-        c.fillStyle = "aliceblue";
-        c.beginPath();
-        c.arc(600, 500, radius, 0, 2 * Math.PI);
-        c.fill();
-        c.stroke();
-
-        // Draw the land.
-        c.lineWidth = 0.35;
-        c.fillStyle = "mintcream";
-        c.beginPath(); 
-        path(result);
-        c.fill();
-        c.stroke();
-
-        //the gratitcule
-        graticule = d3.geoGraticule10()
-        c.beginPath();
-        path(graticule);
-        // c.strokeStyle = "#ccc";
-        c.stroke();
-
-        
+        this.draw(ctx, path, radius, result);
         
         setInterval(() => {
-            this.projection.rotate([this.change(), 0])
-
-            
-            c.lineWidth = 0.7;
-            c.fillStyle = "aliceblue";
-            c.beginPath();
-            c.arc(600, 500, radius, 0, 2 * Math.PI);
-            c.fill();
-            c.stroke();
-
-            
-            c.lineWidth = 0.35;
-            c.fillStyle = "mintcream";
-            c.beginPath();
-            path(result);
-            c.fill();
-            c.stroke();
-
-            graticule = d3.geoGraticule10()
-            c.beginPath();
-            path(graticule);
-            // c.strokeStyle = "#ccc";
-            c.stroke();
-
-            
+            this.projection.rotate([this.change(), 0]);
+            this.draw(ctx, path, radius, result);
         }, 500)
 
     }
+
+    Globe.prototype.draw = function (ctx, path, radius, result) {
+        // Draw the seas.
+        ctx.lineWidth = 1.5;
+        ctx.fillStyle = "aliceblue";
+        ctx.beginPath();
+        ctx.arc(600, 500, radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+
+        // Draw the land.
+        ctx.lineWidth = 0.35;
+        ctx.fillStyle = "mintcream";
+        ctx.beginPath();
+        path(result);
+        ctx.fill();
+        ctx.stroke();
+
+        //the gratitcule
+        graticule = d3.geoGraticule10()
+        ctx.beginPath();
+        path(graticule);
+        // ctx.strokeStyle = "#ccc";
+        ctx.stroke();
+        
+    }
             
-    Globe.prototype.change = function() {
+    Globe.prototype.change = function(degree) {
         this.degree += 10;
-        if (this.degree === 360) {
-            this.degree = 0;
-        }
+        if (this.degree === 360) this.degree = 0;
         return this.degree
     }
 
