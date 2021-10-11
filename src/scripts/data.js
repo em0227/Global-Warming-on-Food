db93f18c0a3a157ab33f325213ca14e5df3fcea8
Emily Wu <emilywu0227@gmail.com>
2021-10-11 18:20:59 -0400

    create 3 years of data for wheat, and adjust css styling for other crops data

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -5,7 +5,7 @@ const margin = { top: 10, right: 30, bottom: 30, left: 60};
 const width = 460; - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-const svg = d3.select(".globe-data")
+const svg = d3.select(".wheat")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
@@ -17,7 +17,7 @@ const svg = d3.select(".globe-data")
 // let i = 0
 
 async function buildData () {
-    let data = await d3.csv("./src/data/A1F1_2020.csv");
+    let data = await d3.csv("./src/data/crops-yield-changes.csv");
     createData(data)
 }
 
@@ -34,7 +34,7 @@ function createData (data) {
         .call(d3.axisBottom(x));
 
     const y = d3.scaleLinear()
-        .domain([-10, 10])
+        .domain([-25, 20])
         .range([height, 0]);
     
     svg.append("g")
@@ -46,10 +46,32 @@ function createData (data) {
         .enter()
         .append("circle")
             .attr("cx", function (d) { return x(d.Country);} )
-            .attr("cy", function (d) { return y(d.Change);} )
+            .attr("cy", function (d) { return y(d.WHA1F2020);} )
             .attr("r", 5)
             .style("fill", "#69b3a2")
             .style("stroke", "white");
+
+    svg.append("g")
+        .selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.Country); })
+        .attr("cy", function (d) { return y(d.WHA1F2050); })
+        .attr("r", 5)
+        .style("fill", "scarlet")
+        .style("stroke", "white");
+
+    svg.append("g")
+        .selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.Country); })
+        .attr("cy", function (d) { return y(d.WHA1F2080); })
+        .attr("r", 5)
+        .style("fill", "red")
+        .style("stroke", "white");
 
 
 }
