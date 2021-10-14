bc171c2b93ad20f3b7d113dd4b4c39970aa63bce
Emily Wu <emilywu0227@gmail.com>
2021-10-14 10:38:43 -0400

    fixed typo

--- dist/main.js
+++ dist/main.js
@@ -1133,9 +1133,10 @@ eval("var global = __webpack_require__(/*! ../internals/global */ \"./node_modul
 /*!***********************!*\
   !*** ./src/index.css ***!
   \***********************/
-/***/ (function() {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n    ╷\n112 │   back\n    │       ^\n    ╵\n  src/index.css 112:7  root stylesheet\n    at tryRunOrWebpackError (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/HookWebpackError.js:88:9)\n    at __webpack_require_module__ (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4897:12)\n    at __webpack_require__ (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4854:18)\n    at /Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4925:20\n    at symbolIterator (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/neo-async/async.js:3485:9)\n    at done (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/neo-async/async.js:3527:9)\n    at Hook.eval [as callAsync] (eval at create (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/tapable/lib/Hook.js:18:14)\n    at /Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4832:43\n    at symbolIterator (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/neo-async/async.js:3482:9)\n-- inner error --\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n    ╷\n112 │   back\n    │       ^\n    ╵\n  src/index.css 112:7  root stylesheet\n    at Object.<anonymous> (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/sass-loader/dist/cjs.js!/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/src/index.css:1:7)\n    at /Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:432:11\n    at Hook.eval [as call] (eval at create (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\n    at Hook.CALL_DELEGATE [as _call] (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/tapable/lib/Hook.js:14:14)\n    at /Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4899:39\n    at tryRunOrWebpackError (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/HookWebpackError.js:83:7)\n    at __webpack_require_module__ (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4897:12)\n    at __webpack_require__ (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4854:18)\n    at /Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/webpack/lib/Compilation.js:4925:20\n    at symbolIterator (/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/neo-async/async.js:3485:9)\n\nGenerated code for /Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/node_modules/sass-loader/dist/cjs.js!/Users/emilywu227/Desktop/aA/JS Project/Gobal Warming on Food/src/index.css\n1 | throw new Error(\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: expected \\\"{\\\".\\n    ╷\\n112 │   back\\n    │       ^\\n    ╵\\n  src/index.css 112:7  root stylesheet\");");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguY3NzLmpzIiwibWFwcGluZ3MiOiI7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2dsb2JhbC13YXJtaW5nLW9uLWZvb2QvLi9zcmMvaW5kZXguY3NzPzgwNmUiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/index.css\n");
 
 /***/ }),
 
@@ -1186,6 +1187,17 @@ eval("/**\n * Copyright (c) 2014-present, Facebook, Inc.\n *\n * This source cod
 /******/ 				if (typeof window === 'object') return window;
 /******/ 			}
 /******/ 		})();
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
 /******/ 	}();
 /******/ 	
 /************************************************************************/
