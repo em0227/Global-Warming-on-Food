168ec942001cac8997b60f1932e06d35742e164c
Emily Wu <emilywu0227@gmail.com>
2021-12-06 18:03:26 -0500

    refactor globe updatedata method and update readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Ever wonder how global warming is affecting food supply around the world?
 
-Food supply is one of the more critial impacts of global warming. As the world become warmer, some countries may end up producing more food, while other's yield rate will decrease. Take a look at how the major crops (wheat, rice, maize) yield rate will be affected in the next 60 years and find out your own answers of the question.
+Food supply is one of the more critical impacts of global warming. As the world becomes warmer, some countries may end up producing more food, while others' yield rate will decrease. Take a look at how the major crops (wheat, rice, maize) yield rate will be affected in the next 60 years and find out your own answers to the question.
 
 Live Site [here](https://em0227.github.io/Global-Warming-on-Food/)
 
@@ -23,17 +23,76 @@ Live Site [here](https://em0227.github.io/Global-Warming-on-Food/)
 
 ## Core Features:
 
-- <b>A Self-turning Interactive Globe!</b>
+### \* A Self-turning Interactive Globe!
 
-  user can hover over the globe and it will stop turning. User can then choose a specific country. In the near future, another feature will be user can click on a specific country and see the country's crop yield changes over the years.
+Users can hover over the globe and it will stop turning. Users can then choose a specific country. In the near future, users will be able to click on a specific country and see the country's crop yield changes over the years.
 
-- <b>Data on the Global Crop Yield Changes</b>
+```javascript
+Globe.prototype.rotate = function () {
+  const countries = document.getElementsByClassName("country");
+  const globe = this;
 
-  Based on the CO2 scenario, year, and crop user chooses, the scatter plot will render different results, and user can observe the changes.
+  this.setIntervalId = setInterval(() => {
+    globe.changeRotationAngle();
+    for (let i = 0; i < countries.length; i++) {
+      const country = d3.select(countries[i]);
+      country
+        .attr("class", "country rotate-true")
+        .attr(
+          "d",
+          d3.geoPath(
+            globe.projection.rotate([globe.rotationDegree, globe.axisDegree])
+          )
+        );
+    }
+  }, 50);
+};
+```
 
-- <b>Responsive Design for Mobile Users</b>
+### \* Data on the Global Crop Yield Changes
 
-  the website is optimized for mobile user as well!
+Based on the CO2 scenario, year, and crop the users choose, the globe and the scatter plot will render different results, and users can observe the changes.
+
+```javascript
+Globe.prototype.updateData = async function (crop, scenario, year) {
+  const g = d3.select(".countries");
+  const globe = this;
+  let cropData = await d3.csv("./src/data/crops-yield-changes.csv");
+  g.selectAll(".country").style("fill", function (d) {
+    const currentCountry = cropData.find(
+      (country) => country["CountryName"] === this.id
+    );
+    if (currentCountry) {
+      const val = currentCountry[`${crop}${scenario}${year}`];
+      for (let i = 0; i < globe.yieldMarks.length; i++) {
+        if (val >= globe.yieldMarks[i]) {
+          return globe.yieldColors[i];
+        }
+      }
+    } else {
+      return "gray";
+    }
+  });
+};
+```
+
+### \* Responsive Design for Mobile Users
+
+The website is optimized for mobile users as well!
+
+```
+@media only screen and (max-width: 500px) {
+    h1 {
+        font-size: 25px;
+    }
+
+    .globe-container {
+        flex-direction: column;
+    }
+
+    //...
+}
+```
 
 <br>
 
--- src/scripts/globe.js
+++ src/scripts/globe.js
@@ -1,281 +1,293 @@
+const ControlEvents = require("./control_panel");
 
 function Globe() {
+  const radius = 400;
+  this.axisDegree = 0;
+  this.rotationDegree = 0;
+  this.projection = d3
+    .geoOrthographic()
+    .scale(radius)
+    .precision(0.2)
+    .translate([600, 500]);
+  this.setIntervalId = 0;
+  this.yieldColors = [
+    "purple",
+    "navy",
+    "royalblue",
+    "skyblue",
+    "white",
+    "yellow",
+    "orange",
+    "orangered",
+    "crimson",
+  ];
+  this.yieldMarks = [20, 15, 10, 5, 0, -5, -10, -15, -20];
 
-    const radius = 400;
-    this.axisDegree = 0
-    this.rotationDegree = 0;
-    this.projection = d3.geoOrthographic().scale(radius).precision(0.2).translate([600, 500]);
-    this.setIntervalId = 0;
-    this.yieldColors = ["purple", "navy", "royalblue", "skyblue", "white", "yellow", "orange", "orangered", "crimson"];
-    this.yieldMarks = [20, 15, 10, 5, 0, -5, -10, -15, -20];
+  this.svg = d3
+    .select(".globe")
+    .append("svg")
+    .attr("class", "globe-map")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("viewBox", "100 0 1000 1100");
 
-    this.svg = d3.select(".globe")
-        .append("svg")
-        .attr("class", "globe-map")
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .attr("viewBox", "100 0 1000 1100");
+  this.colorRecG = this.svg
+    .append("g")
+    .attr("class", "color-info")
+    .attr("transform", `translate(0, 950)`);
 
-    this.colorRecG = this.svg
-        .append("g")
-        .attr("class", "color-info")
-        .attr("transform", `translate(0, 950)`);
-    
-    this.colorRecG
-        .append("text")
-        .style('font-size', "20px")
-        .attr("class", "globe-color-info-title")
-        .attr("transform", `translate(500, 0)`)
-        .text("WH A1F 2020")
-    
+  this.colorRecG
+    .append("text")
+    .style("font-size", "20px")
+    .attr("class", "globe-color-info-title")
+    .attr("transform", `translate(500, 0)`)
+    .text("WH A1F 2020");
 }
 
 Globe.prototype.draw = async function () {
-    const map = await this.grabMapData();
-    const cropData = await d3.csv("./src/data/crops-yield-changes.csv");
-    this.createGlobe(map, cropData);
-    this.rotate();
-    this.events();
-}
+  const map = await this.grabMapData();
+  const cropData = await d3.csv("./src/data/crops-yield-changes.csv");
+  this.createGlobe(map, cropData);
+  this.rotate();
+  this.events();
+};
 
 Globe.prototype.grabMapData = async function () {
-    const world = await fetch("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(res => res.json())
-    return topojson.feature(world, world.objects.countries);
-    
-}
+  const world = await fetch(
+    "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
+  ).then((res) => res.json());
+  return topojson.feature(world, world.objects.countries);
+};
 
 Globe.prototype.createGlobe = function (map, cropData) {
+  const g = this.svg
+    .append("g")
+    .attr("class", "countries")
+    .style("position", "relative");
 
-    const g = this.svg.append("g")
-        .attr("class", "countries")
-        .style("position", "relative");
+  const tooltip = d3
+    .select(".globe")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+    .style("position", "absolute");
 
+  const tipMouseover = function (d) {
+    tooltip
+      .style("opacity", 1)
+      .html(this.id)
+      .style("left", this.pageX + 10 + "px")
+      .style("top", this.pageY - 50 + "px");
+  };
 
-    const tooltip = d3.select(".globe")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
-        .style("position", "absolute");
+  const tipMousemove = function (d) {
+    tooltip
+      .html(this.id)
+      .style("left", this.pageX + 10 + "px")
+      .style("top", this.pageY - 50 + "px");
+  };
 
-    const tipMouseover = function (d) {
-        tooltip
-            .style("opacity", 1)
-            .html(this.id)
-            .style("left", (this.pageX + 10) + "px")
-            .style("top", (this.pageY - 50) + "px")
-    }
+  const tipMouseleave = function (d) {
+    tooltip.style("opacity", 0);
+  };
 
-    const tipMousemove = function (d) {
-        tooltip
-            .html(this.id)
-            .style("left", (this.pageX + 10) + "px")
-            .style("top", (this.pageY - 50) + "px")
-    }
+  const globe = this;
 
-    const tipMouseleave = function (d) {
-        tooltip.style("opacity", 0)
-
-    }
-
-    const globe = this;
-
-    g.selectAll("path")
-        .data(map.features)
-        .enter()
-        .append("path")
-        .attr('class', "country rotate-false")
-        .attr("id", d => {
-            return d.properties.name;
-        })
-        .style("fill", function (d) {
-            for (let i = 0; i < cropData.length; i++) {
-                if (d.properties.name === cropData[i].CountryName) {
-                    let val = cropData[i]['WHA1F2020'];
-                    for (let j = 0; j < globe.yieldMarks.length; j++) {
-                        if (val >= globe.yieldMarks[j]) {
-                            return globe.yieldColors[j]
-                        }
-                    }
-                }
+  g.selectAll("path")
+    .data(map.features)
+    .enter()
+    .append("path")
+    .attr("class", "country rotate-false")
+    .attr("id", (d) => {
+      return d.properties.name;
+    })
+    .style("fill", function (d) {
+      for (let i = 0; i < cropData.length; i++) {
+        if (d.properties.name === cropData[i].CountryName) {
+          let val = cropData[i]["WHA1F2020"];
+          for (let j = 0; j < globe.yieldMarks.length; j++) {
+            if (val >= globe.yieldMarks[j]) {
+              return globe.yieldColors[j];
             }
-            return "gray"
-        })
-        .attr("d", d3.geoPath(this.projection))
-        .on("mouseover", tipMouseover)
-        .on("mousemove", tipMousemove)
-        .on("mouseleave", tipMouseleave);
+          }
+        }
+      }
+      return "gray";
+    })
+    .attr("d", d3.geoPath(this.projection))
+    .on("mouseover", tipMouseover)
+    .on("mousemove", tipMousemove)
+    .on("mouseleave", tipMouseleave);
 
-    const graticule = d3.geoGraticule();
+  const graticule = d3.geoGraticule();
 
-    const g2 = this.svg.append("g")
-        .attr("class", "graticule-outline");
+  const g2 = this.svg.append("g").attr("class", "graticule-outline");
 
-    g2.selectAll("path")
-        .data([graticule()])
-        .enter()
-        .append("path")
-        .attr("class", "graticule")
-        .attr("d", d3.geoPath(this.projection));
-    
+  g2.selectAll("path")
+    .data([graticule()])
+    .enter()
+    .append("path")
+    .attr("class", "graticule")
+    .attr("d", d3.geoPath(this.projection));
 
-    let dis = 45
-    this.colorInfo("gray", 10, "No Data")
-    for (let i = 0; i < this.yieldColors.length; i++) {
-        const color = this.yieldColors[i];
-        const num = this.yieldMarks[i];
-        dis += 70;
-        this.colorInfo(color, dis, num);
-    }
-    
-    
-}
+  let dis = 45;
+  this.colorInfo("gray", 10, "No Data");
+  for (let i = 0; i < this.yieldColors.length; i++) {
+    const color = this.yieldColors[i];
+    const num = this.yieldMarks[i];
+    dis += 70;
+    this.colorInfo(color, dis, num);
+  }
+};
 
 Globe.prototype.colorInfo = function (rectColor, dis, num) {
-    
-    colorRect = this.colorRecG
-        .append("rect")
-        .attr("class", `${num}`)
-        .attr("height", 15)
-        .attr("width", 15)
-        .style("fill", rectColor)
-        .style("stroke-width", 1)
-        .style("stroke", 'gray')
-        .attr("transform", `translate(${230 + dis}, 40)`);
+  colorRect = this.colorRecG
+    .append("rect")
+    .attr("class", `${num}`)
+    .attr("height", 15)
+    .attr("width", 15)
+    .style("fill", rectColor)
+    .style("stroke-width", 1)
+    .style("stroke", "gray")
+    .attr("transform", `translate(${230 + dis}, 40)`);
 
-    const condition = function (num) {
-        if (num === "No Data") {
-            return `${num}`;
-        } else {
-            return `${num}%`;
-        }
+  const condition = function (num) {
+    if (num === "No Data") {
+      return `${num}`;
+    } else {
+      return `${num}%`;
     }
-    
-    this.colorRecG
-        .append("text")
-        .attr("transform", `translate(${250 + dis}, 55)`)
-        .style("font-size", "16")
-        .text(condition(num));
+  };
 
-}
+  this.colorRecG
+    .append("text")
+    .attr("transform", `translate(${250 + dis}, 55)`)
+    .style("font-size", "16")
+    .text(condition(num));
+};
 
+Globe.prototype.rotate = function () {
+  const countries = document.getElementsByClassName("country");
+  const globe = this;
 
-Globe.prototype.rotate = function() {
-    const countries = document.getElementsByClassName("country");
-    const globe = this;
+  this.setIntervalId = setInterval(() => {
+    globe.changeRotationAngle();
+    for (let i = 0; i < countries.length; i++) {
+      const country = d3.select(countries[i]);
+      country
+        .attr("class", "country rotate-true")
+        .attr(
+          "d",
+          d3.geoPath(
+            globe.projection.rotate([globe.rotationDegree, globe.axisDegree])
+          )
+        );
+    }
+  }, 50);
+};
 
-    this.setIntervalId = setInterval(() => {      
-        globe.changeRotationAngle();
-        for (let i = 0; i < countries.length; i++) {
-            const country = d3.select(countries[i]);
-            country
-                .attr("class", "country rotate-true")
-                .attr("d", d3.geoPath(globe.projection.rotate([globe.rotationDegree, globe.axisDegree])))
-        }
-    }, 50);
-
-}
-        
-Globe.prototype.changeRotationAngle = function() {
-    this.rotationDegree += 1;
-    if (this.rotationDegree === 360) this.rotationDegree = 0;
-}
+Globe.prototype.changeRotationAngle = function () {
+  this.rotationDegree += 1;
+  if (this.rotationDegree === 360) this.rotationDegree = 0;
+};
 
 Globe.prototype.events = function () {
-    const globeMap = document.querySelector(".countries");
+  const globeMap = document.querySelector(".countries");
 
-    globeMap.addEventListener("mouseover", e => {
-        clearInterval(this.setIntervalId);
-    })
+  globeMap.addEventListener("mouseover", (e) => {
+    clearInterval(this.setIntervalId);
+  });
 
-    globeMap.addEventListener("mouseout", e => {
-        this.rotate();
-    })
+  globeMap.addEventListener("mouseout", (e) => {
+    this.rotate();
+  });
 
-    globeMap.addEventListener("click", e => {
-        const countryBox = document.querySelector(".country-container");
-        countryBox.style.display = "block";
+  globeMap.addEventListener("click", (e) => {
+    const countryBox = document.querySelector(".country-container");
+    countryBox.style.display = "block";
 
-        const countries = d3.selectAll(".country");
+    const countries = d3.selectAll(".country");
 
-        // console.log(countries);
-        // console.log(countries.attr("class"))
-        // console.log(countries[0].dataset.rotateState)
-        
-        // if (countries.attr("class") === "country rotate-true") {
-        //     countries.each((d, i) => d3.select(countries[i]).attr("class", "country rotate-false"));
-        //     clearInterval(this.setIntervalId);
-        // } else {
-        //     this.rotate();
-        // }
-        
-    });
+    // console.log(countries);
+    // console.log(countries.attr("class"))
+    // console.log(countries[0].dataset.rotateState)
 
-}
+    // if (countries.attr("class") === "country rotate-true") {
+    //     countries.each((d, i) => d3.select(countries[i]).attr("class", "country rotate-false"));
+    //     clearInterval(this.setIntervalId);
+    // } else {
+    //     this.rotate();
+    // }
+  });
+};
 
 Globe.prototype.updateData = async function (crop, scenario, year) {
-    const g = d3.select(".countries");
-    const globe = this;
-    let cropData = await d3.csv("./src/data/crops-yield-changes.csv");
-    // console.log(cropData)
-    // console.log("in the update")
-    g.selectAll(".country")
-        .style("fill", function (d) {
-            let i = 0;
-            for (i; i < cropData.length; i++) {
-                //could use find
-                if (this.id === cropData[i]["CountryName"]) {
-                    let val = cropData[i][`${crop}${scenario}${year}`];
-                    for (let j = 0; j < globe.yieldMarks.length; j++) {
-                        if (val >= globe.yieldMarks[j]) {
-                            return globe.yieldColors[j];
-                        }
-                    }
-                }
-            }
-            // console.log(this.id)
-            // console.log(i)
-            // console.log(cropData[i-1]["CountryName"])
-            // console.log(cropData[i]["CountryName"])
-            return "gray";
-        });
-}
+  const g = d3.select(".countries");
+  const globe = this;
+  const cropData = await d3.csv("./src/data/crops-yield-changes.csv");
+  // console.log(cropData)
+  // console.log("in the update")
+  g.selectAll(".country").style("fill", function (d) {
+    const currentCountry = cropData.find(
+      (country) => country["CountryName"] === this.id
+    );
+    if (currentCountry) {
+      const val = currentCountry[`${crop}${scenario}${year}`];
+      for (let i = 0; i < globe.yieldMarks.length; i++) {
+        if (val >= globe.yieldMarks[i]) {
+          return globe.yieldColors[i];
+        }
+      }
+    } else {
+      return "gray";
+    }
 
-    
-
+    // let i = 0;
+    // for (i; i < cropData.length; i++) {
+    //   //could use find
+    //   if (this.id === cropData[i]["CountryName"]) {
+    //     let val = cropData[i][`${crop}${scenario}${year}`];
+    //     return globe.yieldMarks.find((el) => el <= val);
+    //   }
+    // }
+    // console.log(this.id)
+    // console.log(i)
+    // console.log(cropData[i-1]["CountryName"])
+    // console.log(cropData[i]["CountryName"])
+  });
+};
 
 module.exports = Globe;
 
+// intersting animation to have later:
+// 1. small rotating
+// svg.append("path")
+//     .attr("d", "M10,110 A120,120 -45 0,1 110 10 A120,120 -45 0,1 10,110")
+//     .attr("fill", "none")
+//     .attr("id", "theMotionPath")
 
-    // intersting animation to have later:
-    // 1. small rotating
-    // svg.append("path")
-    //     .attr("d", "M10,110 A120,120 -45 0,1 110 10 A120,120 -45 0,1 10,110")
-    //     .attr("fill", "none")
-    //     .attr("id", "theMotionPath")
+//for each path do:
+// const motion = d3.select(this)
+//     .append("animateMotion")
+//     .attr("dur", "6s")
+//     .attr("repeatCount", "indefinite")
+//     .attr("rotate", "0")
 
-    //for each path do:
-    // const motion = d3.select(this)
-                //     .append("animateMotion")
-                //     .attr("dur", "6s")
-                //     .attr("repeatCount", "indefinite")
-                //     .attr("rotate", "0")
+// motion.append("mpath")
+//     .attr("href", "#theMotionPath")
 
-                // motion.append("mpath")
-                //     .attr("href", "#theMotionPath")
+// 2. rotate outside the screen
+// d3.select(this).transition().duration(5000)
+//     .attrTween("transform", rotTween);
 
-
-    // 2. rotate outside the screen
-    // d3.select(this).transition().duration(5000)
-                //     .attrTween("transform", rotTween);
-
-                // function rotTween() {
-                //     var i = d3.interpolate(0, 360);
-                //     return function (t) {
-                //         return "rotate(" + i(t) + ",50,50)";
-                //     };
-                // }
+// function rotTween() {
+//     var i = d3.interpolate(0, 360);
+//     return function (t) {
+//         return "rotate(" + i(t) + ",50,50)";
+//     };
+// }
